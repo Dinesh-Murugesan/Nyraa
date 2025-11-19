71b0669e799db360f1e1771f2321548a942b8e0c
Dinesh-Murugesan <dineshmurugesan16@gmail.com>
2025-11-19 14:14:37 +0530

    Initial Commit

--- client-business/src/components/Social/Social.tsx
+++ client-business/src/components/Social/Social.tsx
@@ -3,24 +3,24 @@ import { SlSocialFacebook, SlSocialInstagram, SlSocialLinkedin, SlSocialTwitter 
 
 export const Social = () =>
     <div className="flex justify-end p-4 bg-red-100">
-        <div>
+        <div className="mr-5">
             <Link href="https://www.instagram.com">
-                <SlSocialInstagram size={15} fill="#F875AA" className="mr-5" />
+                <SlSocialInstagram size={15} fill="#F875AA" />
             </Link>
         </div>
-        <div>
+        <div className="mr-5">
             <Link href="https://www.facebook.com">
-                <SlSocialFacebook size={15} fill="#F875AA" className="mr-5" />
+                <SlSocialFacebook size={15} fill="#F875AA" />
             </Link>
         </div>
-        <div>
+        <div className="mr-5">
             <Link href="https://www.twitter.com">
-                <SlSocialTwitter size={15} fill="#F875AA" className="mr-5" />
+                <SlSocialTwitter size={15} fill="#F875AA"  />
             </Link>
         </div>
-        <div>
+        <div className="mr-5">
             <Link href="https://www.linkedin.com">
-                <SlSocialLinkedin size={15} fill="#F875AA" className="mr-5" />
+                <SlSocialLinkedin size={15} fill="#F875AA" />
             </Link>
         </div>
     </div>
